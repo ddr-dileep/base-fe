08d3539a3212398a60d5bec82add79836ac61043
Dileep <ddr.dileepraloti@gmail.com>
2025-01-28 19:08:03 +0530

    fix : some issue

--- src/design-system/auth/Login.tsx
+++ src/design-system/auth/Login.tsx
@@ -5,7 +5,11 @@ import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useLoginMutation } from "../../store/auth/authApi";
 import { useAppDispatch, useAppSelector } from "../../store";
-import { clearError, setCredentials, setError } from "../../store/auth/authSlice";
+import {
+  clearError,
+  setCredentials,
+  setError,
+} from "../../store/auth/authSlice";
 import Input from "../common/Input";
 import Button from "../common/Button";
 
@@ -43,14 +47,9 @@ const Login: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Use email: john@mail.com / password: changeme
-          </p>
-        </div>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
 
         <Formik
           initialValues={{ email: "", password: "" }}
